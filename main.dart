ad6dea38cf094cf38919a15a5dc3b528885dc066
Trần Anh Sơn <nos121213@gmail.com>
2026-04-01 16:35:34 +0700

    cấu hình app + routing
    Trần Anh Sơn

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/landing_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "FlixFilm",
       theme: ThemeData.dark(),
-      home: const LandingScreen(), // 👈 QUAN TRỌNG
+      home: LoginScreen(), 
     );
   }
 }
